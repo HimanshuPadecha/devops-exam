fe6a04a535a3c15e965f1558727378edf71d774a
himanshupadecha <himanshupadecha@gmail.com>
2026-05-02 09:53:14 +0530

    removed inputs

--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,6 @@
 import pytest
 from calc import calculator
 
-a = input("Enter no")
-b = input("Enter no")
-
-operation = input("Enter operation : ")
-
 
 assert calculator(10,20,"+") == 30
 assert calculator(20,10,"-") ==  10
