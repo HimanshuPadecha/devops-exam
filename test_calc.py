fae163cb9c56f614f9abb9944d2bc5852191c147
himanshupadecha <himanshupadecha@gmail.com>
2026-05-02 09:57:15 +0530

    added fun

--- test_calc.py
+++ test_calc.py
@@ -1,8 +1,8 @@
 import pytest
 from calc import calculator
 
-
-assert calculator(10,20,"+") == 30
-assert calculator(20,10,"-") ==  10
-assert calculator(20,10,"*" ) == 200
-assert calculator(30,10,"/") == 3
+def test_answer():
+    assert calculator(10,20,"+") == 30
+    assert calculator(20,10,"-") ==  10
+    assert calculator(20,10,"*" ) == 200
+    assert calculator(30,10,"/") == 3
